102b52afa553266cf1028b0de204cb902195acb6
Raj <kmrrajj@gmail.com>
2018-03-23 00:46:41 +0530

    added CampaignFactory contract

--- Campaign.sol
+++ Campaign.sol
@@ -1,5 +1,13 @@
 pragma solidity ^0.4.17;
 
+contract CampaignFactory {
+  address[] public deployedCampaigns;
+  function createCampaign(uint minimum) public {
+    address newCampaign = new Campaign(minimum, msg.sender);
+    deployedCampaigns.push(newCampaign);
+  }
+}
+
 contract Campaign {
     struct Request {
         string description;
